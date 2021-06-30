c81e051215e469ad5ce4a3f3cfb7262dbc976cc5
Nick <nicholasmcdonald40@gmail.com>
2021-07-01 01:36:29 +0200

    Added a sigkill handler, added the help and info commands now finally, also ddynamically loaded class destruction works correctly now. no more memory leaking. additionally, piping data into files works as input now

--- splash/splash.h
+++ splash/splash.h
@@ -2,10 +2,14 @@
 #include <chrono>
 #include <vector>
 #include <unistd.h>
+#include <csignal>
 
 #include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
 namespace fs = boost::filesystem;
+
+const fs::path home = fs::path(getenv("HOME"));
+const fs::path root = home/".config/splash";
 
 #include "helper/logger.h"
 #include "helper/parse.h"
@@ -15,12 +19,14 @@ namespace fs = boost::filesystem;
 #include "include/event.cpp"
 #include "include/program.cpp"
 
-/*
-    Splash 2020
-    Author: Nicholas McDonald
-*/
-
-//lmao
-void printhelp(){
-  logger::raw("splash 2020 by Nicholas McDonald");
+void sighandler(int signal){
+  event::active = false;
 }
+
+const std::string info =
+  #include "resource/info.txt"
+;
+
+const std::string help =
+  #include "resource/help.txt"
+;
