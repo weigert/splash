77928e0d37e506351f0dcecedf8a27f035829144
Nick <nicholasmcdonald40@gmail.com>
2021-07-01 01:36:58 +0200

    program file now tracks its own pipe and has a correct destructor handling

--- splash/include/program.cpp
+++ splash/include/program.cpp
@@ -2,38 +2,64 @@
 #include <string>
 #include <functional>
 #include <iostream>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 using svec = std::vector<std::string>;
 
 #include <dlfcn.h>
 
-#ifndef PROGRAM_CPP
-#define PROGRAM_CPP
+#ifndef SPLASH_PROGRAM
+#define SPLASH_PROGRAM
 
 class Program {
 public:
+
   Program(svec* s){
+
     data = s;
+
+    ppipe = (root/"pipe"/spipe::next()).string();
+    mkfifo(ppipe.c_str(), 0666);
+
+    fpipe = open(ppipe.c_str(), O_RDONLY | O_NONBLOCK);
+    if(fpipe < 0) logger::fatal("Failed to Open Pipe");
+    logger::raw(ppipe);
+
   }
 
   virtual ~Program(){
+
     delete data;
+    close(fpipe);
+    if(remove(ppipe.c_str()) != 0)
+      logger::raw("Failed to delete pipe");
+
   }
 
   svec* data;
+  std::string ppipe;
+  int fpipe;          //Pipe File Handle
 
-  virtual void pipeline() = 0;
-  virtual void event(event::eventdata&) = 0;
+  virtual void pipeline() = 0;                  //Rendering Pipeline
+  virtual void event(event::eventdata&) = 0;    //Event Handler
+  virtual void onpipe(std::string) = 0;         //Piped Data Handler
 
+  static void* hndl;
   static Program* get(std::string, svec* s, parse::data* d);
+  static void destroy(Program* p);
+
 };
 
+// Constructor and Destructor Factories
+
+void* Program::hndl;
 typedef Program* (*create_t)(svec* s, parse::data* d);
 typedef void destroy_t(Program*);
 
 Program* Program::get(std::string f, svec* s, parse::data* d){
 
-	void *hndl = dlopen(f.c_str(), RTLD_NOW);
+	hndl = dlopen(f.c_str(), RTLD_NOW);
   if(hndl == NULL){
     std::cout<<dlerror()<<std::endl;
     return NULL;
@@ -48,6 +74,18 @@ Program* Program::get(std::string f, svec* s, parse::data* d){
   }
 
 	return t(s, d);
+}
+
+void Program::destroy(Program* p){
+
+  destroy_t* destroyer = (destroy_t*)dlsym(hndl, "destroy");
+	const char *dlsym_error = dlerror();
+	if (dlsym_error) {
+		cerr << "Cannot load symbol destroy: " << dlsym_error << '\n';
+		return;
+	}
+
+  destroyer(p);
 }
 
 #endif
